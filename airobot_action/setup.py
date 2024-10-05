04b9cf001d161ee01d853fb266d930b966125e6c
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2024-10-06 07:29:06 +0900

    ros2 pkg create

---
+++ airobot_action/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'airobot_action'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='masutani',
+    maintainer_email='masutani@isc.osakac.ac.jp',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'bringme_action_client_node = airobot_action.bringme_action_client_node:main',
+            'bringme_action_server_node = airobot_action.bringme_action_server_node:main',
+        ],
+    },
+)
